be0b377c0fe4b48201d53be73791e56e3faccc9d
Sternblitz <info@sternblitz.de>
2025-11-27 02:51:46 +0100

    Update video link and page title

--- src/App.jsx
+++ src/App.jsx
@@ -34,7 +34,7 @@ const VideoModal = ({ isOpen, onClose }) => {
           <iframe
             width="100%"
             height="100%"
-            src="https://www.youtube.com/embed/6e6fB-B423M?autoplay=1&rel=0"
+            src="https://www.youtube.com/embed/m8dIv_RYRro?autoplay=1&rel=0"
             title="YouTube video player"
             frameBorder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
